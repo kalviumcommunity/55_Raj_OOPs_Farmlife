716f886e34ed80e45d8f30e0ed5bf233ec608314
rajmalpure04 <rajmalpure14@gmail.com>
2024-10-04 23:34:46 +0530

    pointer

--- Farm.cpp
+++ Farm.cpp
@@ -8,14 +8,14 @@ public:
     string cropType;
     int quantity;
 
-    void plantCrops(string type, int qty) {
-        cropType = type;
-        quantity = qty;
-        cout << "You have planted " << quantity << " " << cropType << " crops. They will be ready to harvest in 5 days." << endl;
+    void plantCrops(string cropType, int quantity) {
+        this->cropType = cropType;
+        this->quantity = quantity;
+        cout << "You have planted " << this->quantity << " " << this->cropType << " crops. They will be ready to harvest in 5 days." << endl;
     }
 
     void allocateWater(int liters) {
-        cout << "You allocated " << liters << " liters of water to your crops. The " << cropType << " is growing well." << endl;
+        cout << "You allocated " << liters << " liters of water to your crops. The " << this->cropType << " is growing well." << endl;
     }
 };
 
@@ -24,15 +24,15 @@ public:
     string animalType;
     int quantity;
 
-    void feedAnimals(string type, int qty) {
-        animalType = type;
-        quantity = qty;
-        cout << "You have fed " << quantity << " " << animalType << ". They will produce milk tomorrow." << endl;
+    void feedAnimals(string animalType, int quantity) {
+        this->animalType = animalType;
+        this->quantity = quantity;
+        cout << "You have fed " << this->quantity << " " << this->animalType << ". They will produce milk tomorrow." << endl;
     }
 
     void sellProduce(string produce, int qty, int pricePerUnit) {
         int totalPrice = qty * pricePerUnit;
-        cout << "You sold " << qty << " " << produce << " for Rs. " << totalPrice << ". Your total money is now Rs. " << (totalPrice + 100) << "." << endl;
+        cout << "You sold " << qty << " " << produce << " for $" << totalPrice << ". Your total money is now $" << (totalPrice + 100) << "." << endl;
     }
 };
 
@@ -40,8 +40,8 @@ int main() {
     Crop wheatCrop;
     Animal cow;
 
-    wheatCrop.plantCrops("wheat", 10);
-    cow.feedAnimals("cows", 5);
+    wheatCrop.plantCrops("wheat", 10);   
+    cow.feedAnimals("cows", 5);          
     wheatCrop.allocateWater(20);
     cow.sellProduce("eggs", 10, 15);
 
